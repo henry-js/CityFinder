add47c96cb8e82b69f59aee36042b3aabf0f1efe
henry-js <79054685+henry-js@users.noreply.github.com>
2021-06-01 16:42:29 +0100

    Removed unnecessary using statements

--- CityProcessor.cs
+++ CityProcessor.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
-using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace CityFinder
--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Net.Http;
-using System.Net.Http.Json;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Globalization;
 using System.Linq;
